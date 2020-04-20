4d4a62aca10861bb24741dcd04a206bc0d661a69
EMRE.KAYMAZ <emre.kaymaz@enerjisauretim.com>
2020-04-21 00:00:36 +0300

    First Commit

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    public int score = -1;
+    public TextMeshProUGUI text;
+    private void Start()
+    {
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+        GameManager.OnCubeSpawned += GameManager_OnCubeSpawned;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnCubeSpawned -= GameManager_OnCubeSpawned;
+
+    }
+
+    private void GameManager_OnCubeSpawned()
+    {
+        score++;
+        text.text = "Score: " + score;
+    }
+}
